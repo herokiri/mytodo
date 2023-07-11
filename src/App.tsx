5e1f5603166e1ac58c27c086709e705b3c37f9df
Sergey <herokiri@mail.ru>
2023-07-12 00:45:39 +0300

    commit1207

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
-  const todos = [
-    {
-      id: '1',
-      title: 'buy broad',
-      isCompleted: false,
-    },
-    {
-      id: '2',
-      title: 'сходить погулять ',
-      isCompleted: false,
-    },
-    {
-      id: '3',
-      title: 'встретиться с друзьями',
-      isCompleted: false,
-    },
-    {
-      id: '4',
-      title: 'купить молоко',
-      isCompleted: false,
-    },
-    {
-      id: '5',
-      title: 'закончить вуз',
-      isCompleted: false,
-    }
-  ]
+  
 
   return (
     <div className="App h-screen bg-gray-900">
@@ -37,7 +11,8 @@ function App() {
         <h1 className='p-10 text-white font-semibold text-xl'>
           Todos
         </h1>
-        <TodoList data={todos}/>
+
+        <TodoList data={[]} />
       </div>
     </div>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,14 +4,33 @@ import { title } from 'process'
 
 interface ITodoList {
     data: {
-        id: string, 
+        id: string,
         title: string,
         isCompleted: boolean,
     }[],
 }
 
-const TodoList: FC<ITodoList> = ({data}) => {
+const TodoList: FC<ITodoList> = ({ data }) => {
     const [todos, setTodos] = useState(data)
+
+    const [inputValue, setInputValue] = useState('')
+
+    const createTodo = (title: string, e: any) => {
+        e.preventDefault()
+        const copy = [...todos]
+        setTodos(
+            [
+                {
+                    id: Date.now() + '',
+                    title: title,
+                    isCompleted: false,
+                },
+                ...copy
+            ]
+        )
+        setInputValue('')
+
+    }
 
     const deleteTodo = (id: string) => {
         setTodos(todos.filter(t => t.id !== id))
@@ -19,6 +38,17 @@ const TodoList: FC<ITodoList> = ({data}) => {
 
     return (
         <div>
+            <form>
+                <input className='post flex bg-gray-800 p-7 rounded-3xl text-white mb-6 w-full' type='text' placeholder='Что надо сделать ....'
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                ></input>
+                <button
+                    onClick={(e) => createTodo(inputValue, e)}
+                >
+
+                </button>
+            </form>
             {todos.map(t =>
                 <Todo key={t.id} id={t.id} title={t.title} isCompleted={t.isCompleted} deleteTodo={deleteTodo} />
             )}
