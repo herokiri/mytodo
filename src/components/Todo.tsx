3658881bab4f6de0e465a4e7b6e81865e3f5f640
Sergey <herokiri@mail.ru>
2023-07-12 21:45:04 +0300

    commit120721

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -10,6 +10,8 @@ interface IPost {
 
 const Todo: FC<IPost> = ({ id, title, isCompleted, deleteTodo }) => {
     const [completed, setCompleted] = useState(isCompleted)
+    const [showModal, setShowModal] = React.useState(false);
+
 
     return (
         <div className="post flex bg-gray-800 p-7 rounded-3xl text-white mb-6">
@@ -31,12 +33,76 @@ const Todo: FC<IPost> = ({ id, title, isCompleted, deleteTodo }) => {
                 </p>
             }
 
-            <button
-                onClick={() => deleteTodo(id)}
-                className='ml-auto'
-            >
-                <BsTrash3Fill />
-            </button>
+            {!completed ?
+                <button
+
+                    onClick={() => setShowModal(true)}
+                    className='ml-auto'
+                >
+                    <BsTrash3Fill />
+                </button> :
+                <button
+
+                    onClick={() => deleteTodo(id)}
+                    className='ml-auto'
+                >
+                    <BsTrash3Fill className='bg-red-900' />
+                </button>
+            }
+            {showModal ? (
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold">
+                                        Modal Title
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                        </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                        Вы уверены, что хотите удалить невыполненное задание ?
+                                    </p>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                    <button
+                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => 
+                                            {setShowModal(false)
+                                            deleteTodo(id)}
+                                        }
+                                    >
+                                        Save Changes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
         </div>
     )
 }
